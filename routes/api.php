4dd7a10fc9ed83c4f3793c4dd4c8904722e1b45a
shazzad.hossain <shazzad.hossain@bracits.com>
2025-07-27 03:03:51 +0600

    duplicate route fix for deployment

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Http\Controllers\API\BookApiController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    Route::apiResource('books', BookApiController::class);
-    
+    Route::apiResource('books', BookApiController::class)->names([
+        'index' => 'api.books.index',
+        'store' => 'api.books.store',
+        'show' => 'api.books.show',
+        'update' => 'api.books.update',
+        'destroy' => 'api.books.destroy',
+    ]);
 });
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,11 @@
 use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/', [BookController::class, 'index']);
+
 
 Route::resource('books', BookController::class);
