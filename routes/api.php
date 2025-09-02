1e8bf8119268376420d11eb5efd23d8aa03835d9
shazzad.hossain <shazzad.hossain@bracits.com>
2025-09-02 17:38:19 +0600

    book controller removed

--- routes/api.php
+++ routes/api.php
@@ -10,35 +10,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
 
-    // Route::get('common-data/type/{type}', [CommonDataController::class, 'getCommonData']);
-    Route::post('common-data', [CommonDataController::class, 'getCommonData']);
-    Route::post('common-data/create', [CommonDataController::class, 'store']);
-
-    Route::post('common-data/regions', [CommonDataController::class, 'getRegions']);
-
-    Route::get('common-data/master', [CommonDataController::class, 'getMasterData']);
-
-    Route::get('common-data/{id}', [CommonDataController::class, 'fetchById']);
-
+    // Public routes
     Route::prefix('auth')->group(function () {
         Route::post('register', [AuthController::class, 'register']);
         Route::post('login', [AuthController::class, 'login']); 
-
-        Route::middleware('auth:api')->group(function () {
-            Route::post('logout', [AuthController::class, 'logout']);
-            Route::get('user', [AuthController::class, 'me']);
-            Route::post('refresh', [AuthController::class, 'refresh']);
-        });
     });
 
+    // Protected routes
     Route::middleware('auth:api')->group(function () {
-        Route::apiResource('books', BookApiController::class)->names([
-            'index' => 'api.books.index',
-            'store' => 'api.books.store',
-            'show' => 'api.books.show',
-            'update' => 'api.books.update',
-            'destroy' => 'api.books.destroy',
-        ]);
-    });
+        
+        Route::post('auth/logout', [AuthController::class, 'logout']);
+        Route::get('auth/user', [AuthController::class, 'me']);
+        Route::post('auth/refresh', [AuthController::class, 'refresh']);
 
+        Route::post('common-data', [CommonDataController::class, 'getCommonData']);
+        Route::post('common-data/create', [CommonDataController::class, 'store']);
+        Route::post('common-data/regions', [CommonDataController::class, 'getRegions']);
+        Route::get('common-data/master', [CommonDataController::class, 'getMasterData']);
+        Route::get('common-data/{id}', [CommonDataController::class, 'fetchById']);
+
+    });
 });
